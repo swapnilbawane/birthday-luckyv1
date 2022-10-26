3a1e2f830c24e507c9c51f57491ab86093d87750
swapnilbawane <swapnilbawane08@gmail.com>
2022-10-26 11:56:24 +0530

    add images below result

--- app.js
+++ app.js
@@ -6,43 +6,47 @@ const btnCheck = document.querySelector("#btn-check");
 
 const hiddenText = document.querySelector("#hidden-text"); 
 
+// Refactor: 
 const bodyMain = document.querySelector(".b");
-console.log(bodyMain);
-
 const bodyNew = document.querySelector("body");
+const imageResult = document.querySelector("#image-result"); 
+
 
 let sum = 0; 
 
 function checkBirthdayLucky ()
 { 
     {
-        // before you begin you validate 
-        console.log(dateInput.value);
-        console.log(luckyNumber.value);
+       
         
         if( dateInput.value&&luckyNumber.value>0)
         
         { 
-            //  1. read string and store string without - 
             let birthDateString = dateInput.value.replaceAll("-","");
-         console.log(birthDateString);
         
-            // 2. Add all numbers in string to sum value 
+          
             for ( let index=0; index<birthDateString.length; index++)
             { 
                 sum = sum + Number(birthDateString.charAt(index));     
             }
-            // 3. Find if number is lucky or not? 
-            if(sum%luckyNumber.value===0) { 
+        
+            if(sum%luckyNumber.value===0) 
+            { 
                 hiddenText.style.display = "block";
                 hiddenText.innerText = "Your Birthday is Lucky"; 
                 hiddenText.style.color = "white";
                 hiddenText.style.backgroundColor = "green";
+
                 bodyNew.style.backgroundColor = "white";
-                bodyNew.style.backgroundImage = "url(assets/party4.png)";
+                // bodyNew.style.backgroundImage = "url(assets/party4.png)";
+                imageResult.style.backgroundImage = "url(assets/party4.png)";
+                imageResult.style.backgroundRepeat = "no-repeat";
+                imageResult.style.textAlign = "center";
                 bodyMain.style.backgroundColor = "white";
             }
-            else { 
+
+            else 
+            { 
                 hiddenText.style.display = "block";
                 hiddenText.innerText = "Oops. Never Mind. Birthdate is not matching with lucky number."; 
                 hiddenText.style.color = "white";
@@ -50,7 +54,8 @@ function checkBirthdayLucky ()
                 bodyNew.style.backgroundColor = "white";
                 // bodyNew.style.backgroundImage = "url(assets/sorry.png)";
                 bodyMain.style.backgroundColor = "white";
-                }
+                bodyMain.style.color = "white";
+            }
             
         }
         
